776dffc7e1446ceaf368065251b0cfbcda0ce765
estenioalves <estenioalves94@gmail.com>
2023-06-19 18:03:12 -0300

    criando fila base

--- fila_normal.py
+++ fila_normal.py
@@ -1,17 +1,7 @@
-class FilaNormal:
-    codigo : int = 0
-    fila = []
-    clientes_atendidos = []
-    senha_atual: str = ''
-
+from fila_base import FilaBase
+class FilaNormal(FilaBase):
     def gera_senha_atual(self) -> None:
         self.senha_atual = f'NM {self.codigo}'
-
-    def reseta_fila(self) -> None:
-        if self.codigo >= 100:
-            self.codigo = 0
-        else:
-            self.codigo += 1
 
     def atualiza_fila(self) -> None:
         self.reseta_fila()
